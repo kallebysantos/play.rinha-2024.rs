ee0244aa5a53fe8e8dbff6a449f02dfe2332481a
kallebysantos <kalleby_santos@hotmail.com>
2025-07-06 17:28:18 +0100

    stamp: creating db seed

---
+++ seed.sql
@@ -0,0 +1,6 @@
+insert into clients values (1, 100000, 0);
+insert into clients values (1, 80000, 0);
+insert into clients values (1, 1000000, 0);
+insert into clients values (1, 10000000, 0);
+insert into clients values (1, 500000, 0);
+
